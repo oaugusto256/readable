6f771f3a41070310e9d41f6f726916ddea0792e7
Otavio Augusto <tavioalves@gmail.com>
2019-01-09 23:36:01 -0200

    refactor: Componentization of the post component

---
+++ src/components/Post.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FaThumbsUp, FaThumbsDown, FaEdit, FaTrash } from 'react-icons/fa';
+
+const Post = (props) => {
+  const { post } = props;
+
+  const day = Date(post.timestamp*1000).substring(8,10);
+  const month =  Date(post.timestamp*1000).substring(4,7);
+  const year =  Date(post.timestamp*1000).substring(11,15);
+
+  return (
+    <div key={post.id} className="post">
+      <div className="flex justify-content-between">
+        <p className="post-category">{post.category}</p>
+        <div className="flex">
+          <div className="post-menu-icon mr-2">
+            <FaEdit />
+          </div>
+          <div className="post-menu-icon">
+            <FaTrash />
+          </div>
+        </div>
+      </div>
+
+      <p className="post-title">{post.title}</p>
+      <p className="post-body">{post.body}</p>
+      <p className="post-author">{post.author}</p>
+      <p className="post-date">{`${month} ${day}, ${year}`}</p>
+
+      <div className="mt-2 flex justify-content-between">
+        <div className="flex align-items-center">
+          <div className="post-vote-icon">
+            <FaThumbsUp />
+          </div>
+          <span className="post-vote-score">{post.voteScore}</span>
+          <div className="post-vote-icon">
+            <FaThumbsDown />
+          </div>
+        </div>
+        <p className="post-comments-count">{`${post.commentCount} comments`}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Post;
--- src/containers/readable.js
+++ src/containers/readable.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Navbar from '../components/Navbar';
 import Loading from '../components/Loading';
-import { FaThumbsUp, FaThumbsDown, FaEdit, FaTrash } from 'react-icons/fa';
+import Post from '../components/Post';
 import { connect } from "react-redux";
 import { getPosts } from '../actions/PostAction';
 import { getComments } from '../actions/CommentsAction';
@@ -15,45 +15,7 @@ class Readable extends Component {
     return (
       <>
         {this.props.posts.map(post => {
-          const day = Date(post.timestamp*1000).substring(8,10);
-          const month =  Date(post.timestamp*1000).substring(4,7);
-          const year =  Date(post.timestamp*1000).substring(11,15);
-
-          console.log(post);
-
-          return (
-            <div key={post.id} className="post">
-              <div className="flex justify-content-between">
-                <p className="post-category">{post.category}</p>
-                <div className="flex">
-                  <div className="post-menu-icon mr-2">
-                    <FaEdit />
-                  </div>
-                  <div className="post-menu-icon">
-                    <FaTrash />
-                  </div>
-                </div>
-              </div>
-
-              <p className="post-title">{post.title}</p>
-              <p className="post-body">{post.body}</p>
-              <p className="post-author">{post.author}</p>
-              <p className="post-date">{`${month} ${day}, ${year}`}</p>
-
-              <div className="mt-2 flex justify-content-between">
-                <div className="flex align-items-center">
-                  <div className="post-vote-icon">
-                    <FaThumbsUp />
-                  </div>
-                  <span className="post-vote-score">{post.voteScore}</span>
-                  <div className="post-vote-icon">
-                    <FaThumbsDown />
-                  </div>
-                </div>
-                <p className="post-comments-count">{`${post.commentCount} comments`}</p>
-              </div>
-            </div>
-          )
+          return <Post post={post} />
         })}
       </>
     )
