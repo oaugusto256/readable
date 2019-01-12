dafcd3bf05c4bfe6e7deec9b6f11198b965d8a72
Otavio Augusto <tavioalves@gmail.com>
2019-01-12 19:38:09 -0200

    feat: Install react-modal lib and add a minimal example to edit post feat

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { FaThumbsUp, FaThumbsDown, FaEdit, FaTrash } from 'react-icons/fa';
+import ReactModal from 'react-modal';
 
 export default class Post extends Component {
   state = {
     showEdit: false,
     showDeleteModal: false
+  }
+
+  handleOpenEdit = () => {
+    this.setState({ showEdit: true });
+  }
+
+  handleCloseEdit = () => {
+    this.setState({ showEdit: false });
   }
 
   render() {
@@ -15,37 +24,50 @@ export default class Post extends Component {
     const year =  Date(post.timestamp*1000).substring(11,15);
 
     return (
-      <div key={post.id} className="post">
-        <div className="flex justify-content-between">
-          <p className="post-category">{post.category}</p>
-          <div className="flex">
-            <div className="post-menu-icon mr-2">
-              <FaEdit />
-            </div>
-            <div className="post-menu-icon">
-              <FaTrash />
-            </div>
-          </div>
-        </div>
-
-        <p className="post-title">{post.title}</p>
-        <p className="post-body">{post.body}</p>
-        <p className="post-author">{post.author}</p>
-        <p className="post-date">{`${month} ${day}, ${year}`}</p>
-
-        <div className="mt-2 flex justify-content-between">
-          <div className="flex align-items-center">
-            <div className="post-vote-icon" onClick={() => votePost(post.id, "upVote")}>
-              <FaThumbsUp />
-            </div>
-            <span className="post-vote-score">{post.voteScore}</span>
-            <div className="post-vote-icon" onClick={() => votePost(post.id, "downVote")}>
-              <FaThumbsDown />
+      <>
+        <div key={post.id} className="post">
+          <div className="flex justify-content-between">
+            <p className="post-category">{post.category}</p>
+            <div className="flex">
+              <div className="post-menu-icon mr-2">
+                <div onClick={this.handleOpenEdit}>
+                  <FaEdit />
+                </div>
+              </div>
+              <div className="post-menu-icon">
+                <FaTrash />
+              </div>
             </div>
           </div>
-          <p className="post-comments-count">{`${post.commentCount} comments`}</p>
+
+          <p className="post-title">{post.title}</p>
+          <p className="post-body">{post.body}</p>
+          <p className="post-author">{post.author}</p>
+          <p className="post-date">{`${month} ${day}, ${year}`}</p>
+
+          <div className="mt-2 flex justify-content-between">
+            <div className="flex align-items-center">
+              <div className="post-vote-icon" onClick={() => votePost(post.id, "upVote")}>
+                <FaThumbsUp />
+              </div>
+              <span className="post-vote-score">{post.voteScore}</span>
+              <div className="post-vote-icon" onClick={() => votePost(post.id, "downVote")}>
+                <FaThumbsDown />
+              </div>
+            </div>
+            <p className="post-comments-count">{`${post.commentCount} comments`}</p>
+          </div>
         </div>
-      </div>
+        <div>
+          <ReactModal
+            isOpen={this.state.showEdit}
+          >
+          <div>
+            <button onClick={this.handleCloseEdit}>Close Modal</button>
+          </div>
+            </ReactModal>
+        </div>
+      </>
     )
   }
 }
